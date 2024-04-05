425e70adc233ce1416c72558bf9ced9a2078c97f
ArshdeepBrar300 <arshdeepbrar171@gmail.com>
2024-04-06 01:50:12 +0530

    consoleLogStatement2

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,11 +2,14 @@ import axios from 'axios';
 
 const API_URL='https://gmailserver-j0ib.onrender.com'
 const API_GMAIL=async(urlObject,payload,type)=>{
-    console.log(payload);
-    return await fetch(`${API_URL}/${urlObject.endpoint}/${type}`,{
-        method: urlObject.method,
-        body:payload
-    })
+   
+    if(urlObject.method=='POST'){
+        return await fetch(`${API_URL}/${urlObject.endpoint}/${type}`,{
+            method: urlObject.method,
+            body:payload
+        })
+    }
+    return await fetch(`${API_URL}/${urlObject.endpoint}/${type}`)
 
 }
 
